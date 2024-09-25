8bbda81d73f6cd8126a3f366e65ea5254b153e05
Gunnvalder <kudlukporter@gmail.com>
2024-09-25 10:01:11 -0300

    added 2 moving obstacles

---
+++ Assets/Obsticle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticle : MonoBehaviour
+{
+
+    public float speed = 1;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointA = transform.position = new Vector3(7, 0.5f, 12);
+        pointB = transform.position = new Vector3(7, 0.5f, -12);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
+}
---
+++ Assets/Obsticle2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticle2 : MonoBehaviour
+{
+
+    public float speed = 1;
+    Vector3 pointD;
+    Vector3 pointE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointD = transform.position = new Vector3(-7, 0.5f, 12);
+        pointE = transform.position = new Vector3(-7, 0.5f, -12);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointD, pointE, time);
+    }
+}
